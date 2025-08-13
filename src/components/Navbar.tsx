743d40cdf3eece6dac879ed8e4430d93fd1ea45f
TheDarkLord100 <miracleyt404@gmail.com>
2025-08-14 00:28:33 +0530

    Fixed linting issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="bg-gray-800 px-6 py-4 flex justify-between items-center">
       <div className="text-xl font-bold">Radiant Metals</div>
       <ul className="flex gap-6 text-sm">
-        <li><a href="/" className="hover:text-orange-400">Home</a></li>
-        <li><a href="/products" className="hover:text-orange-400">Products</a></li>
-        <li><a href="/blogs" className="hover:text-orange-400">Blogs</a></li>
-        <li><a href="/about" className="hover:text-orange-400">About Us</a></li>
-        <li><a href="/contact" className="hover:text-orange-400">Contact</a></li>
+        <li><Link href="/" className="hover:text-orange-400">Home</Link></li>
+        <li><Link href="/products" className="hover:text-orange-400">Products</Link></li>
+        <li><Link href="/blogs" className="hover:text-orange-400">Blogs</Link></li>
+        <li><Link href="/about" className="hover:text-orange-400">About Us</Link></li>
+        <li><Link href="/contact" className="hover:text-orange-400">Contact</Link></li>
       </ul>
     </nav>
   );
