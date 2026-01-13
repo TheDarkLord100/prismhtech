624ac661e663a19509430b02518d16e4af116245
TheDarkLord100 <miracleyt404@gmail.com>
2026-01-13 09:48:45 +0530

    Added layout

---
+++ src/app/auth/callback/layout.tsx
@@ -0,0 +1,12 @@
+import { Suspense } from "react";
+export default function AuthCallBackLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </Suspense>
+  );
+}
